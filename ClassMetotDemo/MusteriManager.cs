1325325766d44f96b3a9d7b72b9e5b23cdce1992
Yücel Bilal <yucel.bilal@hotmail.com>
2021-01-20 20:18:53 +0300

    Müşteri Takibi

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        public void MusteriEkle(Musteri musteri)
+        {
+            Console.WriteLine(musteri.Adi + " " + musteri.Soyadi + " Müşterisi Eklendi.");
+        }
+
+        public void MusteriListele(Musteri musteri)
+        {
+            Console.WriteLine(musteri.Adi + " " + musteri.Soyadi + " Müşterisi Listelendi.");
+        }
+
+        public void MusteriSil(Musteri musteri)
+        {
+            Console.WriteLine(musteri.Adi + " " + musteri.Soyadi + " Müşterisi Siindi.");
+        }
+    }
+}
--- ClassMetotDemo/Program.cs
+++ ClassMetotDemo/Program.cs
@@ -6,7 +6,38 @@ namespace ClassMetotDemo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Musteri musteri1 = new Musteri();
+            musteri1.Adi = "Emir";
+            musteri1.Soyadi = "Uyğun";
+            musteri1.Yasi = 21;
+
+            Musteri musteri2 = new Musteri();
+            musteri2.Adi = "Mert";
+            musteri2.Soyadi = "Er";
+            musteri2.Yasi = 20;
+
+            Musteri[] musteriler = new Musteri[] { musteri1, musteri2 };
+
+            foreach (Musteri musteri in musteriler)
+            {
+                Console.WriteLine(musteri.Adi);
+                Console.WriteLine(musteri.Soyadi);
+                Console.WriteLine(musteri.Yasi);
+                Console.WriteLine("-----------------");
+            }
+
+
+            MusteriManager musterimanager = new MusteriManager();
+            musterimanager.MusteriEkle(musteri1);
+            musterimanager.MusteriListele(musteri1);
+            musterimanager.MusteriSil(musteri1);
+            musterimanager.MusteriEkle(musteri2);
+            musterimanager.MusteriListele(musteri2);
+            musterimanager.MusteriSil(musteri2);
+
+
+
+            Console.ReadLine();
         }
     }
 }
